f768aba0d18e3020eec3746e80feb61367c1aced
Cesar Augusto <cesaraugustoc@hotmail.com>
2023-11-25 12:58:18 -0300

    Adicionando pesquisa

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,19 @@
 class SearchController < ApplicationController 
+
+    before_action :use_cart
+
     def redirect
-        redirect_to CGI.escape("/busca?termo=#{params[:term]}")
+        final_url = uri_parser.escape "/busca?termo=#{params[:term]}"
+        redirect_to final_url
     end
 
     def index
         term = params[:termo]
         wildcard = "%#{term}%"
         @candies = Candy.where("name LIKE ? OR description LIKE ?", wildcard, wildcard)
+    end
+
+    def uri_parser 
+        @uri_parser ||= URI::Parser.new
     end
 end
